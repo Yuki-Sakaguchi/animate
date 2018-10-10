ff6ad7ab7d605b03a1487644a5ccb927af8ae576
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2018-10-11 00:26:44 +0900

    [modify] animateplus.jsとmo.jsを追加

--- public/js/mo.js
+++ public/js/mo.js
@@ -4,7 +4,15 @@
 (function() {
   // box
   function initBox() {
-
+    var square = document.querySelector('.el.box');
+    new mojs.Tween({
+      repeat: 999,
+      delay: 2000,
+      onUpdate: function (progress) {
+        var bounceProgress = mojs.easing.bounce.out(progress);
+        square.style.transform = 'translateY(' + 100*bounceProgress + 'px)';
+      }
+    }).play();
   }
 
   // 初期化
